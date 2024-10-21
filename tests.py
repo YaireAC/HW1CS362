25e0c7f1068f707401534e4c1628299dc875cbf7
Yaire Aguilar <114464751+YaireAC@users.noreply.github.com>
2024-10-20 23:13:27 -0700

    Update tests.py

--- tests.py
+++ tests.py
@@ -2,6 +2,19 @@ import unittest
 from credit_card_validator import credit_card_validator
 
 class Testcredit_card_validator(unittest.TestCase):
+
+#Return true if valid otherwise false
+#       Visa
+# Prefix(es): 4
+# Length: 16
+#       MasterCard
+# Prefix(es): 51 through 55 and 2221 through 2720 
+# Length: 16
+#       American Express
+# Prefix(es): 34 and 37
+# Length: 15
+
+
     # Example
     # def test11(self):
     # """Verifies if Master Cards with valid lengths and invalid check bits returns False
@@ -9,7 +22,19 @@ class Testcredit_card_validator(unittest.TestCase):
     # self.assertFalse(credit_card_validator("...."))
     
     def test1(self):
-        self.assertTrue(credit_card_validator("4123456789012345"))
+        #Empty
+        self.assertTrue(credit_card_validator(""))
+
+    def test2(self):
+        #length 14
+        self.assertTrue(credit_card_validator("34345678901234"))
+
+    def test3(self):
+        #lnegth 17
+        self.assertTrue(credit_card_validator("34345678901234123"))
+    
+    def test4(self):
+        self.assertTrue(credit_card_validator("34345678901234123"))
 
 
 if __name__ == '__main__':
