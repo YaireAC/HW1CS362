87936536694142a5cc7d14982c80a9426665f28f
Yaire Aguilar <114464751+YaireAC@users.noreply.github.com>
2024-10-21 17:59:26 -0700

    Update tests.py

--- tests.py
+++ tests.py
@@ -3,6 +3,7 @@ from credit_card_validator import credit_card_validator
 
 
 class TestCreditCardValidator(unittest.TestCase):
+    
     """
     Return true if valid otherwise false
     Visa:
@@ -15,63 +16,50 @@ class TestCreditCardValidator(unittest.TestCase):
         Prefix(es): 34 and 37
         Length: 15
     """
-
-
     def test1(self):
         # Empty
         self.assertFalse(credit_card_validator(""))
-
-    
+        
     def test2(self):
         # Length 14
         self.assertFalse(credit_card_validator("34345678901234"))
 
-    
     def test3(self):
         # Doesn't pass algorithm
         self.assertFalse(credit_card_validator("5234567890123456"))
 
-    
     def test4(self):
         # Start with 3 instead of 4
         self.assertFalse(credit_card_validator("3123457890123456"))
 
-    
     def test5(self):
         # 2721 not 2720
         self.assertFalse(credit_card_validator("2721567890123456"))
 
-    
     def test6(self):
         # Length 15
         self.assertFalse(credit_card_validator("222140534324887"))
 
-    
     def test820(self):
         # Just the prefix
         self.assertFalse(credit_card_validator("34"))
 
-    
     def test899(self):
         # Just the prefix
         self.assertFalse(credit_card_validator("37"))
-
     
     def test111(self):
         # Doesn't pass Luhn algorithm
         self.assertFalse(credit_card_validator("341111111111111"))
-
     
     def test121(self):
         # Length 20
         self.assertFalse(credit_card_validator("34345678901234567890"))
 
-    
     def test1121(self):
         # Length 10
         self.assertFalse(credit_card_validator("3434567890"))
 
-    
     def test1321(self):
         # Invalid credit card number
         self.assertFalse(credit_card_validator("12345678901234"))
